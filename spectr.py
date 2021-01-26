f747ac3542a02e99d13e23b18ac94c8bb89fb135
enjoycowboy <eveiga@msn.com>
2021-01-26 10:42:26 -0300

    parser integrado no resto do arquivo

--- spectr.py
+++ spectr.py
@@ -1,42 +1,41 @@
-# bloco de chamar o programa?
-#import subprocess, sys, getopt
-#import numpy as np
+import subprocess, sys, getopt
+import numpy as np
 #argvs estao certos
 
-#def main():
-#    time = int(sys.argv[1])
-#    gain = int(sys.argv[2])
-#    outfile = sys.argv[3]
-#    read_dev(time,gain,outfile)
-#def output():
+def main():
+    time = int(sys.argv[1])
+    gain = int(sys.argv[2])
+    outfile = sys.argv[3]
+    read_dev(time,gain,outfile)
+def output():
 
-#essa parte eu sei que funciona
+essa parte eu sei que funciona
 
-#def read_dev(time,gain,outfile): funcao de acesso ao dispositivo
-#    freq_arg = '-f '+str(898e6)+':'+str(932e6)
-#    gain_arg = '-g '+str(gain)
-#    time_arg = '-e '+str(time)+'m'
-#    file_arg = '-m '+str(outfile)
-#    print ("executando fftw por " + str(time) +" minutos com ganho de "+str(gain/10)+" dB")
-#    subprocess.run(["rtl_power_fftw", freq_arg, "-b 512", gain_arg, time_arg,"-o 66" ,file_args])
-#    parse_meta(outfile)
+def read_dev(time,gain,outfile): funcao de acesso ao dispositivo
+    freq_arg = '-f '+str(898e6)+':'+str(932e6)
+    gain_arg = '-g '+str(gain)
+    time_arg = '-e '+str(time)+'m'
+    file_arg = '-m '+str(outfile)
+    print ("executando fftw por " + str(time) +" minutos com ganho de "+str(gain/10)+" dB")
+    subprocess.run(["rtl_power_fftw", freq_arg, "-b 512", gain_arg, time_arg,"-o 66" ,file_args])
+    parse_meta(outfile)
 
-#def parse_meta(infile): #analise dos metadados do scan
+def parse_meta(infile): #analise dos metadados do scan
+    filedir = './'+infile+'.met'
+    meta = open("binout.met", "r")
+    propval = []
+    propname = [] #isso aqui pode ser criado com os nomes ja
+    for line in meta:
+        field = line.split(" # ")
+        propval.append(field[0])
+        propname.append(field[1].rstrip('\n')) #talvez as propriedades vem sempre na mesma ordem
+    meta.close()
+    props = dict(zip(propname,propval))
+    print(props)
 
-#filedir = './'+infile+'.met'
-meta = open("binout.met", "r")
-propval = []
-propname = []
-for line in meta:
-    field = line.split(" # ")
-    propval.append(field[0])
-    propname.append(field[1].rstrip('\n'))
-meta.close()
-props = dict(zip(propname,propval))
-print(props)
-#    with open(filename, 'rb') as f: isso aqui eh pra puxar as fft
-        #data = np.fromfile(f, dtype=np.float32)
-        #array = np.reshape(data, [11000, 11000])
+    with open(filename, 'rb') as f: isso aqui eh pra puxar as fft
+    data = np.fromfile(f, dtype=np.float32)
+    array = np.reshape(data, [11000, 11000])
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
